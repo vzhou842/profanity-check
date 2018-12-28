c3e03d409d4054d3e2d081bcf2ebdf734ab8e886
Victor Zhou <vzhou842@gmail.com>
2018-12-27 23:41:36 -0600

    version 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="profanity-check",
-  version="0.1.0",
+  version="1.0.0",
   author="Victor Zhou",
   author_email="vzhou842@gmail.com",
   description="A fast, robust library to check for offensive language in strings.",
@@ -13,10 +13,10 @@ setuptools.setup(
   long_description_content_type="text/markdown",
   url="https://github.com/vzhou842/profanity-check",
   packages=setuptools.find_packages(),
-  install_requires=['sklearn==0.20.2']
+  install_requires=['sklearn==0.20.2'],
   package_data={ 'profanity_check': ['data/model.joblib', 'data/vectorizer.joblib'] },
   classifiers=[
-    "Development Status :: 4 - Beta",
+    "Development Status :: 5 - Production/Stable",
     "Natural Language :: English",
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: MIT License",
