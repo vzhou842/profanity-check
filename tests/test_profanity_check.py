7d1f4ef73de027e0847f2004b27679986109c6cc
peter279k <peter279k@gmail.com>
2019-01-04 11:51:38 +0800

    Test enhancement

--- tests/test_profanity_check.py
+++ tests/test_profanity_check.py
@@ -1,28 +1,34 @@
+import unittest
 from profanity_check import predict, predict_prob
 
-def test_accuracy():
-  texts = [
-    'Hello there, how are you',
-    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    '!!!! Click this now!!! -> https://example.com',
-    'fuck you',
-    'fUcK u',
-    'GO TO hElL, you dirty scum',
-  ]
-  assert list(predict(texts)) == [0, 0, 0, 1, 1, 1]
+class test_profanity_check(unittest.TestCase):
+  def test_accuracy(self):
+    texts = [
+      'Hello there, how are you',
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      '!!!! Click this now!!! -> https://example.com',
+      'fuck you',
+      'fUcK u',
+      'GO TO hElL, you dirty scum',
+    ]
+    self.assertListEqual([0, 0, 0, 1, 1, 1], list(predict(texts)))
 
-  probs = predict_prob(texts)
-  for i in range(len(probs)):
-    if i < 3:
-      assert probs[i] <= 0.5
-    else:
-      assert probs[i] >= 0.5
+    probs = predict_prob(texts)
+    for i in range(len(probs)):
+      if i < 3:
+        self.assertGreaterEqual(0.5, probs[i])
+      else:
+        self.assertLessEqual(0.5, probs[i])
 
-def test_edge_cases():
-  texts = [
-    '',
-    '                    ',
-    'this is but a test string, there is no offensive language to be found here! :) ' * 25,
-    'aaaaaaa' * 100,
-  ]
-  assert list(predict(texts)) == [0, 0, 0, 0]
+  def test_edge_cases(self):
+    texts = [
+      '',
+      '                    ',
+      'this is but a test string, there is no offensive language to be found here! :) ' * 25,
+      'aaaaaaa' * 100,
+    ]
+
+    self.assertListEqual([0, 0, 0, 0], list(predict(texts)))
+
+if __name__ == "__main__":
+  unittest.main()
