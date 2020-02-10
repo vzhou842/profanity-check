64ba96a215517c09884de1c4baa52927fa70fd38
Michael <michaelmartinez@me.com>
2020-02-10 12:02:15 -0600

    Replaces Sci Kit Learn with Joblib + SciPy

--- profanity_check/profanity_check.py
+++ profanity_check/profanity_check.py
@@ -1,6 +1,5 @@
 import pkg_resources
-import numpy as np
-from sklearn.externals import joblib
+import joblib
 
 vectorizer = joblib.load(pkg_resources.resource_filename('profanity_check', 'data/vectorizer.joblib'))
 model = joblib.load(pkg_resources.resource_filename('profanity_check', 'data/model.joblib'))
@@ -12,4 +11,4 @@ def predict(texts):
   return model.predict(vectorizer.transform(texts))
 
 def predict_prob(texts):
-  return np.apply_along_axis(_get_profane_prob, 1, model.predict_proba(vectorizer.transform(texts)))
+  return [_get_profane_prob(i) for i in model.predict_proba(vectorizer.transform(texts))]
