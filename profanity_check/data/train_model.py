d40cdfa325ed39253806d0d210d42a834ac2478b
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2021-09-30 10:22:17 +0100

    Styling recommendations.

--- profanity_check/data/train_model.py
+++ profanity_check/data/train_model.py
@@ -1,3 +1,5 @@
+"""Train Model from data"""
+
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
@@ -12,8 +14,8 @@ vectorizer = TfidfVectorizer(stop_words="english", min_df=0.0001)
 X = vectorizer.fit_transform(texts)
 
 model = LinearSVC(class_weight="balanced", dual=False, tol=1e-2, max_iter=1e5)
-cclf = CalibratedClassifierCV(base_estimator=model)
-cclf.fit(X, y)
+calibrated_classifier_cv = CalibratedClassifierCV(base_estimator=model)
+calibrated_classifier_cv.fit(X, y)
 
 dump(vectorizer, "vectorizer.joblib")
-dump(cclf, "model.joblib")
+dump(calibrated_classifier_cv, "model.joblib")
