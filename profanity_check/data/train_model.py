dfa52d3e03c72b0e870a2821f9f3baf04efa9c9d
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2023-07-01 13:48:40 +0100

    Train model: decompress file if not there.
    Also: add code into name == main check.

--- profanity_check/data/train_model.py
+++ profanity_check/data/train_model.py
@@ -1,20 +1,36 @@
 """Train Model from data"""
-import pandas as pd
-from joblib import dump
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
-from sklearn.calibration import CalibratedClassifierCV
+import subprocess
+from pathlib import Path
 
-data = pd.read_csv("clean_data.csv")
-texts = data["text"].astype(str)
-y = data["is_offensive"]
+# import pandas as pd
+# from joblib import dump
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.svm import LinearSVC
+# from sklearn.calibration import CalibratedClassifierCV
 
-vectorizer = TfidfVectorizer(stop_words="english", min_df=0.0001)
-X = vectorizer.fit_transform(texts)
+DATA_FILE_NAME = "clean_data.csv"
 
-model = LinearSVC(class_weight="balanced", dual=False, tol=1e-2, max_iter=int(1e5))
-calibrated_classifier_cv = CalibratedClassifierCV(estimator=model)
-calibrated_classifier_cv.fit(X, y)
+# data = pd.read_csv(DATA_FILE)
+# texts = data["text"].astype(str)
+# y = data["is_offensive"]
+#
+# vectorizer = TfidfVectorizer(stop_words="english", min_df=0.0001)
+# X = vectorizer.fit_transform(texts)
+#
+# model = LinearSVC(class_weight="balanced", dual=False, tol=1e-2, max_iter=int(1e5))
+# calibrated_classifier_cv = CalibratedClassifierCV(estimator=model)
+# calibrated_classifier_cv.fit(X, y)
+#
+# dump(vectorizer, "vectorizer.joblib")
+# dump(calibrated_classifier_cv, "model.joblib")
 
-dump(vectorizer, "vectorizer.joblib")
-dump(calibrated_classifier_cv, "model.joblib")
+if __name__ == "__main__":
+    print()
+    print("Training model")
+    print()
+
+    data_file = Path(__file__).parent / DATA_FILE_NAME
+    if not data_file.exists():
+        print(f"Could not find {DATA_FILE_NAME}, will try to extract.")
+        subprocess.run(["./decompress_data"])
+
