e46991c887f93737133e8edb8ff5879c8f8678c9
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2023-07-01 14:01:45 +0100

    Add sha512 check.

--- profanity_check/data/train_model.py
+++ profanity_check/data/train_model.py
@@ -1,4 +1,5 @@
 """Train Model from data"""
+import hashlib
 import subprocess
 from pathlib import Path
 
@@ -9,6 +10,16 @@ from pathlib import Path
 # from sklearn.calibration import CalibratedClassifierCV
 
 DATA_FILE_NAME = "clean_data.csv"
+HASH_OF_DATA_FILE = f"{DATA_FILE_NAME}.sha512"
+
+
+def sha256sum(filename) -> str:
+    """Compute SHA256 hash of file
+
+    Helper method from StackOverflow: https://stackoverflow.com/a/44873382
+    """
+    with open(filename, 'rb', buffering=0) as f:
+        return hashlib.file_digest(f, "sha512").hexdigest()
 
 # data = pd.read_csv(DATA_FILE)
 # texts = data["text"].astype(str)
@@ -24,6 +35,7 @@ DATA_FILE_NAME = "clean_data.csv"
 # dump(vectorizer, "vectorizer.joblib")
 # dump(calibrated_classifier_cv, "model.joblib")
 
+
 if __name__ == "__main__":
     print()
     print("Training model")
@@ -34,3 +46,17 @@ if __name__ == "__main__":
         print(f"Could not find {DATA_FILE_NAME}, will try to extract.")
         subprocess.run(["./decompress_data"])
 
+        hash_sha512 = sha256sum(data_file)
+        hash_file = Path(__file__).parent / HASH_OF_DATA_FILE
+        stored_hash = hash_file.read_text().strip().split(" ")[0]
+        print(stored_hash)
+
+        print()
+        print(f"SHA512 hash of {DATA_FILE_NAME}: {hash_sha512}")
+        print(f"Stored hash to check against: {stored_hash}")
+        print()
+
+        assert hash_sha512 == stored_hash, (
+            f"Hash of {DATA_FILE_NAME} does not match stored hash. "
+            "Please download the data again."
+        )
